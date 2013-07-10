42c6fc3f67d84687b4b4109f358342dcf916aae2
Nikolay <ypypy28@gmail.com>
2013-07-10 23:29:25 +0400

    complete 54 but line 11 and line 26 are not from the task. It was because of typo after russian translation the book

--- page54/src/Main.java
+++ page54/src/Main.java
@@ -1,31 +1,31 @@
 public class Main{
 
 	public static void main(String[] args) {
-	
+
 		int x = 0;
 		while (x < 4) {
-			x = x - 1
+			x = x - 1;
 			if (x < 1) {
-				System.out.print("a ");		
+				System.out.print("a ");				
 			}
-			x = x + 1;
+			
+			if (x > 1) {
+				System.out.print("an");
+				System.out.print(" oyster");				
+			}
+			
+			if (x == 1) {
+				System.out.print("annoye");
+			}
 			
 			if (x < 1) {
 				System.out.print("noise");
-				x = x + 2;
-			}
-					
-			if (x == 1) {
-				System.out.print("a ");
-			}
-			
-			if (x > 3) {
-				System.out.print(" oyster");
-			}
+				x = x + 1;
+			}	
 			System.out.println("");
-			
+			x = x + 2;
 		}
-	
+		
 	}
 
 
