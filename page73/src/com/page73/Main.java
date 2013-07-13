d603bf5211664076a144ee16f079bd1b3fd29a76
Nikolay <ypypy28@gmail.com>
2013-07-13 13:12:33 +0400

    page73 to go

---
+++ page73/src/com/page73/Main.java
@@ -0,0 +1,20 @@
+package com.page73;
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		DrumKit d = new DrumKit();
+		
+		d.playSnare();
+		d.playTopHat();
+		
+		d.snare = false;
+		
+		if (d.snare == true) {
+			d.playSnare();
+		}
+		
+	}
+
+}
